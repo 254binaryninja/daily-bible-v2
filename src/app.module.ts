5685ff8967fb95940caffabdd079ca0efbaa9a95
254binaryninja <arnoldmusandu@gmail.com>
2024-12-01 19:26:56 +0300

    add the bible,AI and prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BibleModule } from './bible/bible.module';
+import { AiInteractionModule } from './ai-interaction/ai-interaction.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
- 
+  imports: [
+    AuthModule,
+    UserModule,
+    BibleModule,
+    AiInteractionModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
